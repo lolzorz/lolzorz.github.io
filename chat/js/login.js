a16e03e1d0b745bf0e99ef38276fe48c422dd3a1
lozorz <sgqgxjc@126.com>
2015-04-21 22:25:03 +0800

    .

--- chat/js/login.js
+++ chat/js/login.js
@@ -1,4 +1,7 @@
 function submit() {
 	var btn = document.getElementById("username").value;
+	if(!btn) {
+		alert("USERNAME CAN NOT BE NULL");
+	}
 	window.location.replace("./chatroom.html?username=" + btn);
 }
