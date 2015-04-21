0bad4b6b3cace3de8ad54fdbe19d15e2e0e4b5e9
lozorz <sgqgxjc@126.com>
2015-04-21 22:23:52 +0800

    .

--- chat/js/login.js
+++ chat/js/login.js
@@ -1,4 +1,4 @@
 function submit() {
 	var btn = document.getElementById("username").value;
-	window.location.replace("./../chatroom.html?username=" + btn);
+	window.location.replace("./chatroom.html?username=" + btn);
 }
